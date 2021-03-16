1e2629a9a816c574d5346368ccabc2657355e66a
Otabek0780 <mrdunacdtm@gmail.com>
2021-03-16 10:07:09 +0900

    Initial Commit

---
+++ CreasyChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreasyChat
+//
+//  Created by kim jong moon on 16/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreasyChat/CreasyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreasyChatApp.swift
+//  CreasyChat
+//
+//  Created by kim jong moon on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CreasyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
